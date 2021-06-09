ed6827ec732a8a6d81f72aee94cb7dfe4b695ac5
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 10:55:28 +0200

    creation

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+        <title>PHP ex5</title>
+    </head>
+    <body >
+        <?php
+        $x=NULL;
+        echo nl2br("x set as NULL= '".$x."'\n");
+        $x="non null";
+        echo nl2br("x= '".$x."'\n");
+        ?>
+    </body>
+</html>
